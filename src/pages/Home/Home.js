85bc1b5d36926e0ce6231e4c61e2b0ec9ed93025
FlaSh <nexus2493@gmail.com>
2021-04-04 22:02:47 +0900

    material ui 설치

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import '@styles/example.scoped.scss'
+import './Home.scoped.scss';
 const Home = ()=> {
   return(
-    <div className="example">
+    <div className="example example2">
       asdfffd
     </div>
   )
